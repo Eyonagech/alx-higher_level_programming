7c83ea27d5f8799321f3159e1ace26c8c0f12594
Eyonagech <yonchan2520@gmail.com>
2022-06-20 18:26:17 +0300

    excep

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python3
 def safe_print_list (my_list=[], x=0):
-    return x
+    b = 0
+    while True:
+        try:
+            if b < x:
+                print(my_list[b], end='')
+                b += 1
+            else:
+                print()
+                return b
+        except IndexError:
+            print()
+            return b
