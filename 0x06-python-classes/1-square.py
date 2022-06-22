670c4c2765f63df2d3f80ba95bfaf56067715ed7
Eyonagech <yonchan2520@gmail.com>
2022-06-22 09:32:11 +0300

    masd

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
-class square:
-    int n
-    square = n * n
-    return square
+#!/usr/bin/python3
+""" Creates an empty class called Square
+"""
+
+
+class Square:
+    """ Empty class with size private attribute
+    """
+    def __init__(self, size):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        self.__size = size
